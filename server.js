e91601139de050028c33307bc689d125d253fac8
Rafael Sævarssonn <rafaelsaevarssonn@Rafaels-MacBook-Air.local>
2025-02-20 15:01:03 +0000

    breyta server.js uppsetningu

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
-const express = require('express');
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
